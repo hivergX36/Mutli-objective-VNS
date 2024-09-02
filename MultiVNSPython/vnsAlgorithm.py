291f23cf23b2bed88b489aa56aee24768bb833b2
hivergX36 <vic.herve44@gmail.com>
2024-09-03 00:31:30 +0200

    MS_03_09_2024

--- MultiVNSPython/vnsAlgorithm.py
+++ MultiVNSPython/vnsAlgorithm.py
@@ -80,11 +80,10 @@ class vnsAlgorithm():
         self.NbVariable = 0 
         self.NbObjectives = 0 
         self.NbConstraint = 0 
-
         self.PriceVariable = []
         self.Constraint = []
         self.MatrixConstraint = []
-     
+        self.point_choosen = 0 
         self.non_dominated_points = []
         self.neighborhood_points = []
         
@@ -134,6 +133,21 @@ def init_non_dominated_point(self):
                   self.non_dominated_points[i].sumconstraint(self.MatrixConstraint)
                   self.non_dominated_points[i].CheckIndividual()
             self.non_dominated_points[i].calculatefitnessvalue(self.PriceVariable)
+            self.point_choosen = init_non_dominated_point[0]
+            
+             
+def add(self):
+      list_alea = [i for i in range(self.NbVariable) if self.CopySolution[i] == 0]
+      indice = random.choice(list_alea)
+      self.CopySolution[indice] = 1
+      
+def drop(self):
+      list_alea = [i for i in range(self.NbVariable) if self.CopySolution[i] == 1]
+      indice = random.choice(list_alea)
+      self.CopySolution[indice] = 0
+      
+def neightboorhood(self):
+      
             
             
 
@@ -159,33 +173,7 @@ def displaySample(self):
         print("The sample is: ")
         for i in range(self.NbInd):
               print(self.Sample[i].solution, " ", self.Sample[i].fitnessValue1, self.Sample[i].fitnessValue2, self.Sample[i].rank)
-              
-def ranknon_dominated_points(self):
-        self.non_dominated_points.sort(key = lambda x: x.fitnessValue1, reverse = True)
-        self.non_dominated_points[0].rank = 0 
-        for i in range(1,self.NbPop):
-              if  self.non_dominated_points[i].fitnessValue2 <  self.non_dominated_points[i - 1].fitnessValue2 and self.non_dominated_points[i].fitnessValue1 < self.non_dominated_points[i - 1].fitnessValue1: 
-                    self.non_dominated_points[i].rank = self.non_dominated_points[i - 1].rank + 1
-              else:
-                    self.non_dominated_points[i].rank = self.non_dominated_points[i - 1].rank
-                    
-def rankList(self):
-        self.List.sort(key = lambda x: x.fitnessValue1, reverse = True)
-        self.List[0].rank = 0 
 
-        for i in range(1,len(self.List)):
-              if  self.List[i].fitnessValue2 <  self.List[i - 1].fitnessValue2 and self.List[i].fitnessValue1 < self.List[i - 1].fitnessValue1: 
-                    self.List[i].rank = self.List[i - 1].rank + 1
-              else:
-                    self.List[i].rank = self.List[i - 1].rank
-  
-def frontList(self):
-        self.List.sort(key = lambda x : x.rank)
-        numberRank = self.List[-1].rank  
-        self.front = [[self.List[i] for i in range(len(self.List)) if self.List[i].rank == r] for r in range(numberRank + 1)]
-        print("Bonjour2", self.front)
-        while len(self.front[-1]) < 1:
-              self.front = self.front[0:(len(self.front) - 1)]
         
          
                     
@@ -200,34 +188,55 @@ def define_non_dominated_pointsfront(self):
         
       
         
-        
-
-                    
-  
-  
 def Update_non_dominated_points(self):
       compteur = 0 
+      max = 0 
+      ind_max = 0 
       arret = False 
-      self.List = [self.non_dominated_points[i] for i in range(self.NbPop)]
-      for i in range(self.NbInd):
-            self.List.append(self.Sample[i])
-      self.rankList()
-      self.frontList()
-      self.measurecrowdingdistance()
-      self.non_dominated_points = []
-      for i in range(len(self.front)):
-              if arret == True:
-                    break 
-              for j in range(len(self.front[i])):
-                    self.non_dominated_points.append(self.front[i][j])
-                    compteur =+ 1 
-                    if compteur == self.NbPop:
-                          arret = True
-                          break
+      list_toupdate = []
+      updated_list = []
+      self.non_dominated_points.sort(key = lambda x: x.fitnessValue1, reverse = True)
+      for i in range(len(self.non_dominated_points)):
+            list_toupdate.append(self.non_dominated_points[i])
+      for i in range(len(self.neighborhood_points)):
+            list_toupdate.append(self.neighborhood_points[i])
+      updated_list.append(list_toupdate[0])
+      max = list_toupdate[0].fitnessValue2
+      ind_max = 0 
+      for i in range(1,len(self.List)):
+              if  list_toupdate[i].fitnessValue2 > list_toupdate[ind_max].fitnessValue2 and self.List[i].fitnessValue1 <= self.List[i - 1].fitnessValue1: 
+                    updated_list.append(list_toupdate[i])
+                    max = list_toupdate[i].fitnessValue2 
+                    ind_max = i 
+      self.non_dominated_points = updated_list
                     
-              
-              
-        
+                    
+def Update_neighborhood(self):
+      compteur = 0 
+      max = 0 
+      ind_max = 0 
+      arret = False 
+      list_toupdate = []
+      updated_list = []
+      self.neighborhood_points.sort(key = lambda x: x.fitnessValue1, reverse = True)
+      list_toupdate = []
+      updated_list = []
+      for i in range(len(self.neighborhood_points)):
+            list_toupdate.append(self.neighborhood_points[i])
+      updated_list.append(list_toupdate[0])
+      max = list_toupdate[0].fitnessValue2
+      ind_max = 0 
+      for i in range(1,len(self.List)):
+              if  list_toupdate[i].fitnessValue2 > list_toupdate[ind_max].fitnessValue2 and self.List[i].fitnessValue1 <= self.List[i - 1].fitnessValue1: 
+                    updated_list.append(list_toupdate[i])
+                    max = list_toupdate[i].fitnessValue2 
+                    ind_max = i 
+      self.neighborhood_points = updated_list
+                    
+
+                    
+            
+ 
 
 
   
